035f254d001eddbf312c5814f511e50477d08ac7
Karam Abbas <kma69362@gmail.com>
2025-12-10 19:46:42 -0700

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TestComponent from './tutorial/03-useEffect/3-useEffect-fetch';
+import TestComponent from './tutorial/04-conditional-rendering/1-multiple-returns';
 import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
